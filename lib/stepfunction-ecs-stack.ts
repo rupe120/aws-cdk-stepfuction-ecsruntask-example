5009e3833eca1be62e5fc67539b16b7a12593894
Josh Russo <jrusso@innovativesol.com>
2023-11-05 18:44:32 -0500

    run task example

--- lib/stepfunction-ecs-stack.ts
+++ lib/stepfunction-ecs-stack.ts
@@ -1,16 +1,74 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class StepfunctionEcsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    
+        // Write results to parquet with an ECS Task
+        const taskDefinition = new ecs.FargateTaskDefinition(
+            this,
+            `test-container-task`
+        );
+        const containerImage = ecs.ContainerImage.fromAsset('src/test-container');
+        taskDefinition.addContainer(
+            `test-container`,
+            {
+                image: containerImage,
+                containerName: `test-container`,
+                environment: {
+                    REGION_ID: this.region
+                },
+            }
+        );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StepfunctionEcsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const ecsVpc = new ec2.Vpc(this, 'Vpc', {
+            maxAzs: 3,
+            natGateways: 1,
+        })
+        const ecsCluster = new ecs.Cluster(this, 'Cluster', {
+            vpc: ecsVpc,
+        });
+
+        // Write the results to parquet with an ECS Task
+        const ecsTask = new tasks.EcsRunTask(
+            this,
+            `stepfunction-ecs-task`,
+            {
+                integrationPattern: sfn.IntegrationPattern.RUN_JOB,
+                cluster: ecsCluster,
+                taskDefinition: taskDefinition,
+                launchTarget: new tasks.EcsFargateLaunchTarget(
+                    {
+                        platformVersion: ecs.FargatePlatformVersion.VERSION1_4,
+                    }
+                ),
+                containerOverrides: [
+                    {
+                        environment: [
+                            {
+                                name: "QUERY_EXECUTION_ID",
+                                value: "dummy-query-execution-id",
+                            }
+                        ]
+                    } as tasks.ContainerOverride
+                ],
+                propagatedTagSource: ecs.PropagatedTagSource.TASK_DEFINITION,
+            }
+        );
+        new sfn.StateMachine(
+            this,
+            `state-machine`,
+            {
+                definitionBody: sfn.DefinitionBody.fromChainable(ecsTask),
+                
+            }
+        );
+    }
 }
